9385bd8c1eb6b7570b38d99961f7a37f77ff439e
MiguelosAguialos <aguiarmiguel65@gmail.com>
2022-08-31 17:27:55 -0300

    Commit 1

---
+++ public/scripts.js
@@ -0,0 +1,14 @@
+function onOff(){
+    document
+        .getElementById("modal")
+        .classList
+        .toggle("hide")
+}
+
+const slider = document.querySelector("input")
+const value = document.querySelector(".value")
+value.textContent = slider.value
+slider.oninput = function(){
+    value.textContent = this.value
+}
+
